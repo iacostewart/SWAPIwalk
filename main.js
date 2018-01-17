910f2aefae3a8af106a39ea1fe31d46abfeb7d94
Ian Stewart <iacostewart@msn.com>
2018-01-17 18:58:57 -0500

    modified main.js

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
 $(() => {     //short hand for document ready before start
 //shit goes inbetween here.. 
 
-alert('working')
+$('#infoButton').click(() =>{
+    $.ajax({
+        type: 'GET', 
+        url: 'https://swapi.co/api/people'
+    }).done((res) => {
+        let people = res.results
+        for(person of people) {
+            console.log(person.name)
+        }
+    })
 })
+
+
+})
+
+
+
+
+//console.log(people)
+        //console.log(res.results)
